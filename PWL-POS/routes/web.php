9e49132c843114d0c4b7272024d084c9a44e26f7
Atherizz <saveroathallah86@gmail.com>
2026-02-28 20:26:06 +0700

    docs: finishing report for jobsheet 3

--- PWL-POS/routes/web.php
+++ PWL-POS/routes/web.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
 
 Route::get('/category/{category}', [ProductController::class, 'category']);
 
